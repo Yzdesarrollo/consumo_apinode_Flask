1cf114f673b42c709466c51fd4c2887b730bb1fd
yeison.ariel@accenture.com <yeison.ariel@accenture.com>
2020-06-21 14:54:34 -0500

    Usando db mysql guardando datos

--- index.py
+++ index.py
@@ -1,21 +1,40 @@
-from flask import Flask, render_template # Importaciones
+from flask import Flask, render_template, request, redirect, url_for # Importaciones
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='',
+    database='taskapp'
+)
+
 app = Flask(__name__) # Instancia de la clase con el constructor
 
 @app.route('/') # Decorador modifica el comportamiento de una funcion
-def start():
-    return render_template('home.html')
+def index():
+    sql = 'SELECT * FROM tasks'
+    cur = mydb.cursor()
+    cur.execute(sql)
+    result = cur.fetchall()
+    return render_template('home.html', tasks = result)
 
-@app.route('/home') 
-def home():
-    return render_template('home.html')
+@app.route('/createtask') # Decorador modifica el comportamiento de una funcion
+def createtask():
+    return render_template('create-task.html')
 
-@app.route('/login') 
-def login():
-    return render_template('login.html')
+@app.route('/addtask', methods=['POST']) 
+def addtask():
+    if request.method == 'POST':
+        taskName = request.form['task'] # Variable name que viene del formulario 
+        taskDate = request.form['date'] # Variable date que viene del formulario
+        cur = mydb.cursor()
+        sql = f"INSERT INTO tasks (task,date) VALUES ('{taskName}','{taskDate}')"
+        cur.execute(sql)
+        mydb.commit()
+        print('taskName: ',taskName, 'taskDate: ', taskDate)
+        return redirect(url_for('index'))
+    return 'Error'
 
-@app.route('/register') 
-def register():
-    return render_template('register.html')
 
 if __name__== "__main__": # Servidor 
     app.run(debug=True)
